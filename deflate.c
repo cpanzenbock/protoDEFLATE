929eab6d467d9e28a06b91012e7cab751faf6f85
Cameron Panzenbock <cpanzenbock@gmail.com>
2018-05-03 19:18:40 +1000

    pulled out another subfunction and deleted debug printing

--- deflate.c
+++ deflate.c
@@ -18,6 +18,11 @@
 		--- Figure out the longest match length
 		-- Replace the current byte and subsequent bytes with a backreference [D:L] 3B
 		-- Increment number of backreferences and queue the cursor's address
+		-- Record the location of this backreference in the uncompressed file
+		   [This would usually be used by huffman to identify where the backreferences
+		   are, but since we aren't doing huffman right now, these locations need
+		   to be stored with the compressed file so that it's possible to
+		   decompress it (else distinguishing backreferences from data is impossible).]
 
 	Simplified Huffman spec:
 		- Count up frequencies of all characters
@@ -26,10 +31,6 @@
 		We'll not do this for now though.
 
 */
-
-//#define DEBUG_LV0 1
-//#define DEBUG_LV1 1
-//#define DEBUG_LV2 1
 
 #define FILE_MAX_INPUT_SIZE 16777216	// 2^24
 #define LZ_MAX_BACK_REFS 16777216		// 2^24
@@ -56,42 +57,30 @@ void extract_file(char *filename);
 void save_compression(char *filename);
 void lz77();
 bref get_longest_match(uint32_t u_cursor);
+int find_length_of_match(uint32_t u_cursor, uint32_t w_cursor);
 uint8_t get_w1(uint32_t in);
 uint8_t get_w0(uint32_t in);
 void huffman();
 
 int main (int argc, char *argv[]) {
 
-	#ifdef DEBUG_LV0
-	printf("Entering main function...\n");
-	#endif
-
 	extract_file(argv[1]);
 	lz77();
 	huffman();
 	save_compression(argv[2]);
+
 	return EXIT_SUCCESS;
 }
 
 void extract_file(char *filename) {
 
-	#ifdef DEBUG_LV0
-	printf("Extracting file...\n");
-	#endif
-
 	FILE *f = fopen(filename, "r");
 	uncompressed_size = fread(uncompressed, sizeof(uint8_t), FILE_MAX_INPUT_SIZE, f);
 	fclose(f);
 
-	#ifdef DEBUG_LV0
-	printf("Extracting file complete...\n");
-	#endif
 }
 
 void save_compression(char *filename) {
-	#ifdef DEBUG_LV0
-	printf("Saving compression to file [num_backrefs = %d]\n", num_backrefs);
-	#endif
 
 	FILE *f = fopen(filename, "w");
 	fwrite(&lzcompressed_size, sizeof(uint64_t), 1, f);
@@ -100,15 +89,9 @@ void save_compression(char *filename) {
 	fwrite(lzcompressed, sizeof(uint8_t), lzcompressed_size, f);
 	fclose(f);
 
-	#ifdef DEBUG_LV0
-	printf("Saving compression to file compelete...\n");
-	#endif
 }
 
 void lz77() {
-	#ifdef DEBUG_LV0
-	printf("LZ77 commencing...\n");
-	#endif
 
 	uint32_t u_cursor = 0; //uncompressed index
 	uint64_t l_cursor = 0; //lzcompressed index
@@ -132,22 +115,15 @@ void lz77() {
 	}
 	lzcompressed_size = l_cursor;
 
-	#ifdef DEBUG_LV0
-	printf("LZ77 complete...\n");
-	#endif
 }
 
 bref get_longest_match(uint32_t u_cursor) {
-	#ifdef DEBUG_LV0
-	printf("Finding longest match at cursor/char: %d|%c...\n", u_cursor, uncompressed[u_cursor]);
-	#endif
 
 	uint32_t w_cursor; //window index
-	uint32_t m_cursor; //match index
 
 	bref ref;
 	ref.found = 0;
-	ref.length = 7; // Match needs to be greater than 3 to be worth it.
+	ref.length = 7; // Match needs to be greater than 3 (+4 for backreference location at beginning) to be worth it.
 
 	int match_len;
 	uint32_t displacement;
@@ -157,21 +133,8 @@ bref get_longest_match(uint32_t u_cursor) {
 	while (w_cursor < u_cursor) {
 		if (uncompressed[u_cursor] == uncompressed[w_cursor]) {
 
-			#ifdef DEBUG_LV1
-			printf("\tMatched initial '%c' with '%c' @ w_cursor=%d\n", uncompressed[u_cursor], uncompressed[w_cursor], w_cursor);
-			#endif
+			match_len = find_length_of_match(u_cursor, w_cursor);
 
-			match_len = 1;
-			m_cursor = w_cursor + 1;
-			while (m_cursor < u_cursor && uncompressed[u_cursor+match_len] == uncompressed[m_cursor] && match_len < LZ_MAX_MATCH_LEN) {
-
-				#ifdef DEBUG_LV2
-				printf("\t\tMatched next '%c' with '%c' @ m_cursor=%d\n", uncompressed[u_cursor+match_len], uncompressed[m_cursor], m_cursor);
-				#endif
-
-				match_len++;
-				m_cursor++;
-			}
 			if (match_len > ref.length) {
 				ref.found = 1;
 				ref.length = match_len;
@@ -185,25 +148,29 @@ bref get_longest_match(uint32_t u_cursor) {
 		}
 	}
 
-	#ifdef DEBUG_LV0
-	printf("Found longest match with [status, address hi:lo, displacement, length]: %d, %x:%x, %d, %d...\n", ref.found, ref.disp_hi, ref.disp_lo, displacement, ref.length);
-	#endif
-
 	return ref;
 }
 
+int find_length_of_match(uint32_t u_cursor, uint32_t w_cursor) {
+
+	int match_len = 1;
+	uint32_t m_cursor; //match index
+	m_cursor = w_cursor + 1;
+	while (m_cursor < u_cursor && uncompressed[u_cursor+match_len] == uncompressed[m_cursor] && match_len < LZ_MAX_MATCH_LEN) {
+
+		match_len++;
+		m_cursor++;
+	}
+
+	return match_len;
+}
+
 uint8_t get_w0(uint32_t in) { // MSB w3|w2|w1|w0 LSB
-	#ifdef DEBUG_LV0
-	printf("Filtering higher byte...\n");
-	#endif
 
 	return (uint8_t)(in & 0x000000FF);
 }
 
 uint8_t get_w1(uint32_t in) { // MSB w3|w2|w1|w0 LSB
-	#ifdef DEBUG_LV0
-	printf("Filtering lower byte...\n");
-	#endif
 
 	return (uint8_t)((in & 0x0000FF00) >> 8);
 }
